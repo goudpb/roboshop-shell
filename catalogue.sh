a4ba88abb2bf97d0dc5e49be62189118559cab98
goudp <goudpb@gmail.com>
2024-04-23 16:34:14 +0530

    added sample file1

---
+++ catalogue.sh
@@ -0,0 +1,17 @@
+cp catalogue.service /etc/systemd/system/catalogue.service
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+yum install nodejs -y
+useradd roboshop
+mkdir /app
+
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
+cd /app
+unzip /tmp/catalogue.zip
+npm install
+
+yum install mongodb-org-shell -y
+mongo --host MONGODB-SERVER-IPADDRESS </app/schema/catalogue.js
+
+systemctl daemon-reload
+systemctl enable catalogue
+systemctl start catalogue
---
+++ mongo.sh
@@ -0,0 +1,5 @@
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+yum install mongodb-org -y
+# update local IP address
+systemctl enable mongod
+systemctl start mongod
