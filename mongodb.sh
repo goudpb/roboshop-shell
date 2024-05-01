2b6f83406a61b3f0cab192682e02616318851294
goudp <goudpb@gmail.com>
2024-05-01 17:04:48 +0530

    updated catalogue

--- mongodb.sh
+++ mongodb.sh
@@ -1,5 +1,6 @@
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 yum install mongodb-org -y
-#update host IP
+#update local IP
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf
 systemctl enable mongod
 systemctl restart mongod
--- redis.sh
+++ redis.sh
@@ -2,5 +2,6 @@ yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
 yum module enable redis:remi-6.2 -y
 yum install redis -y
 #update local host
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 systemctl enable redis
 systemctl restart redis
