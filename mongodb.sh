af1442b18b4352f93434adfb63f7c839d455f203
goudp <goudpb@gmail.com>
2024-05-02 16:55:56 +0530

    updated catalogue

--- mongodb.sh
+++ mongodb.sh
@@ -1,6 +1,6 @@
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 yum install mongodb-org -y
 #update local IP
-sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf /etc/redis/mongod.conf
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 systemctl enable mongod
 systemctl restart mongod
