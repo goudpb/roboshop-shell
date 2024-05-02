affdec92509d8daadab8a40ceb20890c6e167c11
goudp <goudpb@gmail.com>
2024-05-02 22:01:23 +0530

    updated catalogue

--- catalogue.sh
+++ catalogue.sh
@@ -9,12 +9,12 @@ yum install nodejs -y
 useradd roboshop
 mkdir /app
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
-cd /app
-unzip /tmp/catalogue.zip
-npm install
-yum install mongodb-org-shell -y
+#cd /app
+#unzip /tmp/catalogue.zip
+#npm install
+#yum install mongodb-org-shell -y
 
-mongo --host  mongodb.devopsb74.shop </app/schema/catalogue.js
-systemctl daemon-reload
-systemctl enable catalogue
-systemctl restart catalogue
+#mongo --host  mongodb.devopsb74.shop </app/schema/catalogue.js
+#systemctl daemon-reload
+#systemctl enable catalogue
+#systemctl restart catalogue
