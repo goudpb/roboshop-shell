adb1acede7879a12c9669cf61b071e8bb05b7237
goudp <goudpb@gmail.com>
2024-05-02 22:50:42 +0530

    updated catalogue

--- catalogue.sh
+++ catalogue.sh
@@ -1,29 +1,29 @@
 
 cp catalogue.service /etc/systemd/system/catalogue.service
-echo -e "\e[31m>>>>>>>>>>>>>>> Create mongodb <<<<<<<<<<<<<<\e[0m"
+echo -e "\e[36m>>>>>>>>>>>>>>> Create mongodb <<<<<<<<<<<<<<\e[0m"
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 
 yum module disable nodejs -y
 yum module enable nodejs:18 -y
 
-echo "\e[32m>>>>>>>>>>>>>>> Install Node JS <<<<<<<<<<<<<<\e[0m"
+echo "\e[36m>>>>>>>>>>>>>>> Install Node JS <<<<<<<<<<<<<<\e[0m"
 yum install nodejs -y
 
-echo "\e[32m>>>>>>>>>>>>>>> Create Application user <<<<<<<<<<<<<<\e[0m"
+echo "\e[36m>>>>>>>>>>>>>>> Create Application user <<<<<<<<<<<<<<\e[0m"
 useradd roboshop
 
-echo "\e[32m>>>>>>>>>>>>>>> Create Application Directory <<<<<<<<<<<<<<\e[0m"
+echo "\e[36m>>>>>>>>>>>>>>> Create Application Directory <<<<<<<<<<<<<<\e[0m"
 mkdir /app
 
-echo "\e[32m>>>>>>>>>>>>>>> Download Application Content <<<<<<<<<<<<<<\e[0m"
+echo "\e[36m>>>>>>>>>>>>>>> Download Application Content <<<<<<<<<<<<<<\e[0m"
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
 
-echo "\e[32m>>>>>>>>>>>>>>> Extract Application Content <<<<<<<<<<<<<<\e[0m"
+echo "\e[36m>>>>>>>>>>>>>>> Extract Application Content <<<<<<<<<<<<<<\e[0m"
 cd /app
 unzip /tmp/catalogue.zip
 cd /app
 
-echo "\e[32m>>>>>>>>>>>>>>> Download Node JS Dependencies <<<<<<<<<<<<<<\e[0m"
+echo "\e[36m>>>>>>>>>>>>>>> Download Node JS Dependencies <<<<<<<<<<<<<<\e[0m"
 npm install
 
 echo "\e[32m>>>>>>>>>>>>>>> Install Mongo client <<<<<<<<<<<<<<\e[0m"
